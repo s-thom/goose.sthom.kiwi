4078173e9c99925f483bd9ec4547dfbc98d4b638
Stuart Thomson <me@sthom.kiwi>
2023-01-06 19:33:59 +1300

    Make it honk to specific frequencies
    This is ghastly

--- src/play.ts
+++ src/play.ts
@@ -1,15 +1,41 @@
-let currentIndex = 0;
+let honkIndex = 0;
 const HONKS = document.querySelectorAll<HTMLAudioElement>("audio.audio-honk");
-const QUACK = document.querySelector<HTMLAudioElement>("audio.audio-quack");
+// Did a frequency plot in Audacity, hopefully adjusting by these will give a good result
+const HONK_BASE_FREQS = [671, 674, 672, 671, 668];
+const QUACK = document.querySelector<HTMLAudioElement>("audio.audio-quack")!;
 const BIG_GOOSE = document.querySelector<HTMLButtonElement>("#big-goose")!;
+
+let noteIndex = 0;
+const NOTES = [523.25, 493.88, 440, 392, 349.23, 329.63, 293.66, 261.63].map(
+  (n) => n * 2
+);
 
 // Number of degrees the goose can rotate in each direction
 const GOOSE_CRAZINESS = 15;
 
+function playEndSound() {
+  QUACK.play();
+  noteIndex = 0;
+}
+
 function playSound() {
-  const honk = HONKS[currentIndex]!;
+  if (noteIndex >= NOTES.length) {
+    playEndSound();
+    return;
+  }
+
+  const note = NOTES[noteIndex]!;
+  const honk = HONKS[honkIndex]!;
+  const honkFreq = HONK_BASE_FREQS[honkIndex]!;
+
+  const shiftAmount = note / honkFreq;
+
+  honk.playbackRate = shiftAmount;
+  honk.preservesPitch = false;
   honk.play();
-  currentIndex = (currentIndex + 1) % HONKS.length;
+
+  honkIndex = (honkIndex + 1) % HONKS.length;
+  noteIndex++;
 }
 
 function addActiveStyles() {
