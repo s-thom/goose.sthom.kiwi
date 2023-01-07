01c7828452ae277b31883957317da79d6d219096
Stuart Thomson <me@sthom.kiwi>
2023-01-07 16:53:49 +1300

    Musical goose
    This ended up being less effort than I thought. Thanks random developers on the internet who give their work away for free. I unironically thank you for this, and hold you partially responsible for me making this.

--- src/play.ts
+++ src/play.ts
@@ -1,30 +1,68 @@
+import { getNext, MusicData } from "./midi";
+
+// The joke is terrible, and so is this code.
+// I might clean it up one day, but why would I bother.
+
 let honkIndex = 0;
 const HONKS = document.querySelectorAll<HTMLAudioElement>("audio.audio-honk");
 // Did a frequency plot in Audacity, hopefully adjusting by these will give a good result
 const HONK_BASE_FREQS = [671, 674, 672, 671, 668];
 const QUACK = document.querySelector<HTMLAudioElement>("audio.audio-quack")!;
 const BIG_GOOSE = document.querySelector<HTMLButtonElement>("#big-goose")!;
+const BIG_GOOSE_LABEL =
+  document.querySelector<HTMLParagraphElement>("#big-goose-label")!;
+const SKIP = document.querySelector<HTMLButtonElement>("#skip")!;
 
 let noteIndex = 0;
-const NOTES = [523.25, 493.88, 440, 392, 349.23, 329.63, 293.66, 261.63].map(
-  (n) => n * 2
-);
+let currentSong: MusicData | undefined;
 
 // Number of degrees the goose can rotate in each direction
-const GOOSE_CRAZINESS = 15;
+const GOOSE_CRAZINESS = 5;
 
-function playEndSound() {
-  QUACK.play();
-  noteIndex = 0;
+async function getNextSongData() {
+  const prevSong = currentSong;
+  currentSong = undefined;
+
+  let tries = 0;
+  while (tries < 5) {
+    // eslint-disable-next-line no-await-in-loop
+    const newSong = await getNext();
+    if (!prevSong || newSong.meta.id !== prevSong.meta.id) {
+      currentSong = newSong;
+      return currentSong;
+    }
+
+    // Limit the number of tries to prevent infinite loopiness
+    tries++;
+  }
+
+  currentSong = prevSong;
+  return prevSong;
 }
 
-function playSound() {
-  if (noteIndex >= NOTES.length) {
-    playEndSound();
+async function nextSong() {
+  const song = await getNextSongData();
+
+  if (!song) {
     return;
   }
 
-  const note = NOTES[noteIndex]!;
+  noteIndex = 0;
+  BIG_GOOSE_LABEL.textContent = `Now playing: ${song.meta.emoji} (${song.meta.name})`;
+}
+
+function playEndSound() {
+  QUACK.play();
+}
+
+function playSound() {
+  if (!currentSong || noteIndex >= currentSong.notes.length) {
+    playEndSound();
+    nextSong();
+    return;
+  }
+
+  const note = currentSong.notes[noteIndex]!;
   const honk = HONKS[honkIndex]!;
   const honkFreq = HONK_BASE_FREQS[honkIndex]!;
 
@@ -63,7 +101,7 @@ function queueAnim() {
   if (handle) {
     clearTimeout(handle);
   }
-  handle = setTimeout(() => removeActiveStyles(), 400);
+  handle = setTimeout(() => removeActiveStyles(), 200);
 }
 
 function onMouseDown() {
@@ -79,7 +117,9 @@ function onKey(event: KeyboardEvent) {
 
 export function init() {
   // BIG_GOOSE.addEventListener("click", onClick);
-  BIG_GOOSE.addEventListener("mousedown", onMouseDown);
-  BIG_GOOSE.addEventListener("touchstart", onMouseDown);
+  BIG_GOOSE.addEventListener("pointerdown", onMouseDown);
   BIG_GOOSE.addEventListener("keydown", onKey);
+  SKIP.addEventListener("click", nextSong);
+
+  nextSong();
 }
